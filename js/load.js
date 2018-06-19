ec6d78ea00d4902b5631663d438e42651b40bb46
Lars <l.risse@tevaris.de>
2018-06-19 16:45:45 +0200

    added images and next work on galery

--- js/load.js
+++ js/load.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $(".sidenav").sidenav();
   $("img.lazy").lazyload();
   $(".slider").slider();
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
+    indicators: true
+  });
 });
